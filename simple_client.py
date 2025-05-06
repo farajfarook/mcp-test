a6cff6c9a661669f64b60363be0301db82120ce1
Faraj Farook <farajfarook@gmail.com>
2025-05-07 00:09:08 +1000

    feat: Refactor MCPClient to use async setup for streams and session, enhancing functionality and error handling

--- simple_client.py
+++ simple_client.py
@@ -1,12 +1,52 @@
+import json
 from simple_client.llm_utils import LLMUtils
 from simple_client.mcp_session import MCPClient
+from mcp import ClientSession
+from mcp.client.sse import sse_client
 
-client = MCPClient()
-client.setup_streams_and_session()
 
-llm_utils = LLMUtils()
-llm_utils.setup_model_and_tokenizer()
+async def run_async():
+    client = MCPClient()
+    await client.setup_streams_and_session()
 
-print(llm_utils.generate_response("whats the capital of France?"))
+
+async def run_agent():
+    async with sse_client("http://localhost:8000/sse") as streams:
+        async with ClientSession(*streams) as session:
+            await session.initialize()
+            print("Session initialized")
+            prompts = await session.list_prompts()
+            tools = await session.list_tools()
+            resources = await session.list_resources()
+            for prompt in prompts.prompts:
+                print(f"Prompt: {prompt.name} (NOT SUPPORTED)")
+            for tool in tools.tools:
+                func = {
+                    "type": "function",
+                    "function": {
+                        "name": tool.name,
+                        "description": tool.description,
+                        "parameters": {
+                            "type": "object",
+                            "properties": tool.inputSchema.get("properties"),
+                            "required": tool.inputSchema.get("required"),
+                        },
+                    },
+                }
+                print(f"Tool: {json.dumps(func, indent=2)}")
+            for resource in resources.resources:
+                print(f"Resource: {resource.name} (NOT SUPPORTED)")
+    return
+
+
+# llm_utils = LLMUtils()
+# llm_utils.setup_model_and_tokenizer()
+
+# print(llm_utils.generate_response("whats the capital of France?"))
 # print(generate_response("Hey How are you?", use_tools=True))
 # print(generate_response("What about Germany?", use_tools=True))
+
+if __name__ == "__main__":
+    import asyncio
+
+    asyncio.run(run_agent())
--- simple_client/mcp_session.py
+++ simple_client/mcp_session.py
@@ -4,21 +4,32 @@ from mcp.client.sse import sse_client
 
 
 class MCPClient:
-    def setup_streams_and_session(self):
+    async def setup_streams_and_session(self):
         try:
-            streams = sse_client("http://localhost:8000/sse")
-            self.session = ClientSession(*streams)
-            self.session.initialize()
+            stream = await sse_client("http://localhost:8000/sse")
+            session = ClientSession(stream.read, stream.write)
+            await session.initialize()
+            prompts = await session.list_prompts()
+            tools = await session.list_tools()
+            resources = await session.list_resources()
+            for prompt in prompts.prompts:
+                print(f"Prompt: {prompt['name']}")
+            for tool in tools.tools:
+                func = {
+                    "type": "function",
+                    "function": {
+                        "name": tool["name"],
+                        "description": tool["description"],
+                        "parameters": {
+                            "type": "object",
+                            "properties": tool["inputSchema"]["properties"],
+                            "required": tool["inputSchema"]["required"],
+                        },
+                    },
+                }
+                print(f"Tool: {json.dumps(func, indent=2)}")
+            for resource in resources.resources:
+                print(f"Resource: {resource['name']}")
         except Exception as e:
             print(f"Error initializing streams or session to MCP Server: {e}")
             exit()
-
-    def create_message(self, method_name, params, id=None):
-        message = {"jsonrpc": "2.0", "method": method_name, "params": params, "id": id}
-        return json.dumps(message)
-
-    def send_message(self, message):
-        try:
-            self.session.send(message.encode())
-        except Exception as e:
-            print(f"Error sending message to MCP Server: {e}")
